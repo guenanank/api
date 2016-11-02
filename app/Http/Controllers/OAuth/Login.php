d9ccd9c4c2d5488928a346ccc60da16dba109c6e
guenanank <guenanank@yahoo.com>
2016-11-02 17:43:51 +0700

    setup

--- app/Http/Controllers/OAuth/Login.php
+++ app/Http/Controllers/OAuth/Login.php
@@ -9,19 +9,21 @@ use App\Models\Gateway\Employee as Employees;
 class Login extends \App\Http\Controllers\Controller {
 
     public function index(Request $request) {
+
+        $this->validate($request, [
+            'username' => 'required|exists:gateway.employees,*,employeeId,' . $request->username,
+            'password' => 'required'
+        ]);
+
         $employee = Employees::with('section.departement.division', 'position')->find($request->username);
-        if (collect($employee)->isEmpty()) :
-            return response(['stat' => false, 'msg' => 'Employee not found']);
-        else :
-            if (Hash::check($request->password, $employee->password) == false) :
-                return response(['stat' => true, 'msg' => 'Wrong password']);
-            endif;
 
-            $token = sha1(time());
-            $employee->update(['token' => $token]);
-            return response(['stat' => true, 'token' => $token, 'msg' => $employee]);
-
+        if (Hash::check($request->password, $employee->password) == false) :
+            return response(['password' => ['Wrong password']], 422);
         endif;
+
+        $token = sha1(time());
+        $employee->update(['token' => $token]);
+        return response(['token' => $token, 'msg' => $employee]);
     }
 
 }
